d7d0169e8789a426a50413938e0c7e5899134922
vijay532 <sahaniv66@gmail.com>
2019-09-30 18:51:29 +0530

    linked list using recursion

---
+++ LinkedListReverse.c
@@ -0,0 +1,60 @@
+//gcc 5.4.0
+
+#include  <stdio.h>
+#include <stdlib.h>
+struct node 
+{
+    int data;
+    struct node *next;
+};
+void getRev(struct node* head)
+{
+    if(head==NULL)
+    {
+        return;
+    }
+    getRev(head->next);
+    printf("%d ",head->data);
+}
+void print(struct node* head)
+{
+    if(head==NULL)
+    {
+        return;
+    }
+    printf("%d ",head->data);
+    print(head->next);
+}
+void push(struct node** head_ref, int new_data) 
+{ 
+    struct node* new_node = (struct node*)malloc(sizeof(struct node)); 
+    new_node->data = new_data; 
+    new_node->next = (*head_ref); 
+    (*head_ref) = new_node; 
+    //return ;
+} 
+
+int main()
+{
+    int n,m;
+    scanf("%d",&n);
+    struct node* head = NULL;
+    struct node* p,*q;
+    p = (struct node*)malloc(sizeof(struct node));
+    scanf("%d",&m);
+    p->data = m;
+    p->next = NULL;
+    head = p; 
+    for(int i=1;i<n;i++)
+    {
+        scanf("%d",&m);
+        q=(struct node*)malloc(sizeof(struct node));
+        q->data=m;
+        q->next=NULL;
+        p->next=q;
+        p=p->next;
+        //push(&head,m);
+    }
+    print(head);
+    getRev(head);
+}
